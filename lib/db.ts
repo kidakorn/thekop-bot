a100cbddcaca17e497b70cfb7c0ca1f3b53daeec
KIDAKORN.I <kidakorn.1@gmail.com>
2026-06-14 22:26:57 +0700

    fix: update db.ts to pass pg Pool to PrismaPg adapter

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,6 @@
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
@@ -10,7 +11,8 @@ function createPrismaClient(): PrismaClient {
   if (!connectionString) {
     throw new Error('DATABASE_URL is not set in environment variables')
   }
-  const adapter = new PrismaPg({ connectionString })
+  const pool = new Pool({ connectionString })
+  const adapter = new PrismaPg(pool)
   return new PrismaClient({
     adapter,
     log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
